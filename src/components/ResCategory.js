554318a014fd0ec44d7213d5c2072cba64b8a360
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-12-01 11:39:54 +0530

    category

--- src/components/ResCategory.js
+++ src/components/ResCategory.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import ItemList from './itemList';
+import ItemList from './ItemList.js';
 
 
 const ResCategory = ({data}) => {
     console.log(data);
   return (
     <div>
-        <div className="w-6/12 mx-auto my-6 bg-gray-100 shadow-lg p-4 flex justify-between">
+        <div className="w-6/12 mx-auto my-6 bg-gray-200 shadow-lg p-4 flex justify-between">
         <span className='font-bold text-lg'>{data.title} ({data?.itemCards?.length})</span>
         <span>⬇️</span>
         </div>
